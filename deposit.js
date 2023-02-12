4fbce8b5bc68c82065b01eb0f0ea5ad058888846
IamLikhon <likhonadnan55@gmail.com>
2023-02-12 18:17:17 +0600

    section

--- deposit.js
+++ deposit.js
@@ -1,29 +1,59 @@
 
-document.getElementById('deposit-btn').addEventListener('click',function(){
+// document.getElementById('deposit-btn').addEventListener('click',function(){
     
-    const findDeposit=document.getElementById('input-deposit');
-    const getDepositString=findDeposit.value;
-    const getDeposit= parseFloat(getDepositString);
+//     const findDeposit=document.getElementById('input-deposit');
+//     const getDepositString=findDeposit.value;
+//     const getDeposit= parseFloat(getDepositString);
 
-    // clear::::::::
-    findDeposit.value='';
+//     // clear::::::::
+//     findDeposit.value='';
 
-    if(isNaN(getDeposit)){
-        alert ('Enter a valid Number');
-        return;
-     }
-    const depositTotalElement=document.getElementById('deposit-total');
-    const depositTotalString=depositTotalElement.innerText;
-    const depositTotal= parseFloat(depositTotalString)
+//     if(isNaN(getDeposit)){
+//         alert ('Enter a valid Number');
+//         return;
+//      }
+//     const depositTotalElement=document.getElementById('deposit-total');
+//     const depositTotalString=depositTotalElement.innerText;
+//     const depositTotal= parseFloat(depositTotalString)
    
-    const currentDepositTotal=depositTotal+getDeposit
-    depositTotalElement.innerText=currentDepositTotal;
+//     const currentDepositTotal=depositTotal+getDeposit
+//     depositTotalElement.innerText=currentDepositTotal;
 
-    const findBalanceTotal=document.getElementById('balance-total');
-    const balanceString= findBalanceTotal.innerText;
-    const balanceTotal= parseFloat(balanceString);
+//     const findBalanceTotal=document.getElementById('balance-total');
+//     const balanceString= findBalanceTotal.innerText;
+//     const balanceTotal= parseFloat(balanceString);
 
-    const currentBalanceTotal= balanceTotal + getDeposit;
-    findBalanceTotal.innerText=currentBalanceTotal
+//     const currentBalanceTotal= balanceTotal + getDeposit;
+//     findBalanceTotal.innerText=currentBalanceTotal
         
+// })
+
+document.getElementById('deposit-btn').addEventListener('click',function(){
+    let dorsiInput=document.getElementById('input-deposit');
+    let inputText=dorsiInput.value;
+    let covertNumber=parseFloat(inputText);
+
+
+    // clear;;
+    dorsiInput.value='';
+    if(isNaN(covertNumber)){
+        alert('Type Any Number')
+        return;
+    }
+
+    let dorsiDeposit=document.getElementById('deposit-total');
+    let depositText=dorsiDeposit.innerText;
+    let convertDepositNumber=parseFloat(depositText);
+    
+    let sumDepositToInput=convertDepositNumber + covertNumber;
+    dorsiDeposit.innerText=sumDepositToInput;
+
+
+    let dorsiBalance=document.getElementById('balance-total');
+    let balanceText=dorsiBalance.innerText;
+    let covertBalance=parseFloat(balanceText);
+    
+    let sumBalanceToInput= covertBalance + covertNumber;
+    dorsiBalance.innerText=sumBalanceToInput;
+
 })
--- withdraw.js
+++ withdraw.js
@@ -1,35 +1,71 @@
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+// document.getElementById('btn-withdraw').addEventListener('click', function(){
     
-    const getWithdraw=document.getElementById('withdraw-fild');
-    const withdrawString=getWithdraw.value;
-    const withdrawAmount= parseFloat(withdrawString);
+//     const getWithdraw=document.getElementById('withdraw-fild');
+//     const withdrawString=getWithdraw.value;
+//     const withdrawAmount= parseFloat(withdrawString);
 
-     // clear:::
-     getWithdraw.value='';
+//      // clear:::
+//      getWithdraw.value='';
 
-     if(isNaN(withdrawAmount)){
-        alert ('Enter a valid Number');
+//      if(isNaN(withdrawAmount)){
+//         alert ('Enter a valid Number');
+//         return;
+//      }
+
+
+//     const getWithdrawTotalElement= document.getElementById('total-withdraw');
+//     const previousWithdrawString= getWithdrawTotalElement.innerText;
+//     const previousWithdrawAmount=parseFloat(previousWithdrawString);
+   
+
+//     const getBalanceTotal=document.getElementById('balance-total');
+//     const getBalanceString= getBalanceTotal.innerText;
+//     const getBalanceAmount= parseFloat(getBalanceString);
+
+//     if(withdrawAmount>getBalanceAmount){
+//         alert("Sorry You Can't Withdraw Money")
+//         return;
+//     }
+//     const totalResult=previousWithdrawAmount + withdrawAmount;
+//     getWithdrawTotalElement.innerText=totalResult;
+
+//     const newBalanceTotal= getBalanceAmount - withdrawAmount;
+//     getBalanceTotal.innerText=newBalanceTotal;
+    
+// })
+
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    let dorsiWithdrawInput=document.getElementById('withdraw-fild');
+    let withdrawText= dorsiWithdrawInput.value;
+    let gotNumber =parseFloat(withdrawText);
+    
+    // clear++++=
+    dorsiWithdrawInput.value='';
+    if(isNaN(gotNumber)){
+        alert('Type Number ')
+        return;
+    }
+
+    let dorsiWithdrawBox= document.getElementById('total-withdraw');
+    let gotValueString= dorsiWithdrawBox.innerText;
+    let covertBoxNumber=parseFloat(gotValueString);
+    
+    
+     let dorsiBalanceBox= document.getElementById('balance-total');
+     let balanceStringBox=dorsiBalanceBox.innerText;
+     let convertBalanceNumber= parseFloat(balanceStringBox);
+
+     if(gotNumber>convertBalanceNumber){
+        alert('Tor Baap Er Bank e Eto TK Nai')
         return;
      }
 
+     let sumWithdrawToBoxNumber= covertBoxNumber + gotNumber;
+     dorsiWithdrawBox.innerText=sumWithdrawToBoxNumber;
 
-    const getWithdrawTotalElement= document.getElementById('total-withdraw');
-    const previousWithdrawString= getWithdrawTotalElement.innerText;
-    const previousWithdrawAmount=parseFloat(previousWithdrawString);
-   
+     let findBalanceBoxTotal= convertBalanceNumber - gotNumber;
+     dorsiBalanceBox.innerText=findBalanceBoxTotal;
+     
 
-    const getBalanceTotal=document.getElementById('balance-total');
-    const getBalanceString= getBalanceTotal.innerText;
-    const getBalanceAmount= parseFloat(getBalanceString);
-
-    if(withdrawAmount>getBalanceAmount){
-        alert("Sorry You Can't Withdraw Money")
-        return;
-    }
-    const totalResult=previousWithdrawAmount + withdrawAmount;
-    getWithdrawTotalElement.innerText=totalResult;
-
-    const newBalanceTotal= getBalanceAmount - withdrawAmount;
-    getBalanceTotal.innerText=newBalanceTotal;
-    
 })
